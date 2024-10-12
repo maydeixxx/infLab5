f04768cbd09e54afe6226f49cd47843e9b7fe825
amaevalx <kruki999@yandex.ru>
2024-10-13 03:49:22 +0700

    Создан метод getCarByMark

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main{
+public class main{
     public static void main(String[] args) {
         Car[] cars = new Car[]{
             new Car(1, "BMW", "M5", 2020, "BLUE", 20000.0f, "К994ЕН"),
@@ -10,15 +10,22 @@ public class Main{
             new Car(4, "LEXUS", "ISF", 2011, "BLACK", 12.500f, "С174УТ"),
             new Car(5, "BMW", "M3", 2020, "RED", 20.400f, "Н030НН")
         };
+
+        Car[] bmwCars = getCarByMark(cars, "BMW");
+        System.out.println("BMW cars:");
+        for (Car car : bmwCars){
+            System.out.println(car.getModel());
+        } 
     }
 
-    public static Car[] getCarByMark(Car[] cars, String Mark) {
+    public static Car[] getCarByMark(Car[] cars, String mark) {
         List<Car> result = new ArrayList<>();
         for(Car car : cars) {
-            if(car.getMark().equals(Mark)){
+            if(car.getMark().equals(mark)){
                 result.add(car);
             }
         }
         return result.toArray(new Car[0]);
-    }
+    }    
+    
 }
